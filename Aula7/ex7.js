4c0d41c847337a345729551b13c0aa8ba27858d9
CS15A <146629372+CS15A@users.noreply.github.com>
2026-06-11 20:10:08 +0200

    Code fixed

--- Aula7/ex7.js
+++ Aula7/ex7.js
@@ -1,6 +1,6 @@
-//Funções para o site
+// Funções para o site
 function formatarPreco(valor) {
-    return '${valor.toLocaleString("pt-PT")} MT';
+    return `${valor.toLocaleString("pt-PT")} MT`;
 }
 
 function calcularIVA(preco, taxa=17) {
@@ -8,15 +8,15 @@ function calcularIVA(preco, taxa=17) {
 }
 
 function verificarStock(quantidade) {
-    if(quantidadade === 0) 
+    if(quantidade === 0) 
         return "Esgotado";
     if(quantidade < 5) 
-        return 'Últimas ${quantidade} unidades';
+        return `Últimas ${quantidade} unidades`; // ← backtick
     return "Disponível";
 }
 
 console.log(formatarPreco(3200));
-console.log('IVA: ${formatarPreco(calcularIVA(3200))}');
+console.log(`IVA: ${formatarPreco(calcularIVA(3200))}`); // ← backtick
 console.log(verificarStock(0));
 console.log(verificarStock(3));
 console.log(verificarStock(12));
