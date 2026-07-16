dd727d7591e3e12e8c47086eb6de857497c1cb37
CS15A <146629372+CS15A@users.noreply.github.com>
2026-07-16 14:35:04 +0200

    Storying intormation

--- Aula3/ex2.js
+++
@@ -1,8 +0,0 @@
-const produto = "Pneu ATV 27X9-12";
-const preco = 4500;
-const stock = 7;
-
-console.log('Produto:${produto}');
-console.log('Preço:${preco} MT');
-console.log('Em stock:${stock} unidades');
-console.log('Total em stock:${preco * stock} MT');
